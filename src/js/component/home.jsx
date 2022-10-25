f3e0d76271b04b291a0b3b9630e9b02d21a09b31
MireyaCR <mireya.mcr@gmail.com>
2022-10-25 20:57:41 +0000

    reacttodolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,23 +1,27 @@
 import { element } from 'prop-types';
-import React, { useState,useEffect } from 'react';
+import React, { useState,useEffect, isValidElement } from 'react';
 import { putTask, getTask} from '../myfetch';
 
 const Home = () => {
-	useEffect(()=>{getTask().then(Response=>(setTareas(Response)),[])})
+	useEffect(()=>{getTask().then(async response=>{
+		(setTareas((await response.json()).map((element=>element.label))))
+	},[])})
 	const [tareas, setTareas] = useState([]);
 
 	const handleKeydown = async (event) =>{
-	
-	if (event.key === 'Enter') {
-		setTareas([...tareas, event.target.value])
-		await putTask(tareas)
-	}}
+		if (event.target.value !== '' && event.key === 'Enter') {
+			const nuevasTareas = [...tareas, event.target.value]
+			// setTareas(nuevasTareas)
+			await putTask(nuevasTareas)
+			event.target.value = ''
+		}
+	}
 
 	return (
 
 		<div className="container h-25 mt-5 p-5">
 			<h3>Tasks</h3>
-			<input className="form-control sm mb-3"type="text" placeholder='What needs to be done?' onKeyDown={handleKeydown} />
+			<input className="form-control sm mb-3" type="text" placeholder='What needs to be done?' onKeyDown={handleKeydown} />
 			<ul className="list-group list-group-flush">
 				{tareas.map((element, index) => <li key={index+'li'} className="list-group-item"
 				onMouseEnter={(event)=>{
@@ -26,10 +30,10 @@ const Home = () => {
 				onMouseLeave={(event)=>{
 					event.target.childNodes[1].style.visibility="hidden"
 				}}>{element}
-				<button className="btn btn-outline-danger btn-close" key={index} onClick={(event)=>{
+				<button className="btn btn-outline-danger btn-close" key={index} onClick={async(event)=>{
 					tareas.splice(index,1)
-					setTareas([...tareas])
-					myFetch([...tareas])
+					// setTareas([...tareas])
+					await putTask([...tareas])
 				}}></button>
 				</li>)}	
 			</ul>
